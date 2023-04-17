3ad7c4546970c60481add4bcaa97decf0da0fe99
realzzz <real_z@yeah.net>
2023-04-17 21:41:18 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    reportCompressedSize: false,
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "KoaBrowserBot",
+    },
+  },
+});
